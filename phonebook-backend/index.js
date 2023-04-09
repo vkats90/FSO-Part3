cf7e17cbe9022f5e76e9d943389753e60de799e0
vkats90 <mars77@gmail.com>
2023-04-08 23:19:25 -0600

    exercise 3.8 complete

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let phonebook = [
   {
