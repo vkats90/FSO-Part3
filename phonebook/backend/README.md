5b518d3e5c42b7298da329f1e129468975376b85
vkats90 <mars77@gmail.com>
2023-04-10 19:57:13 -0600

    Changed the port to read the .env fild

---
+++ phonebook/backend/README.md
@@ -0,0 +1,5 @@
+## Phonebook app Backend
+
+This is the backend for the phonebook application from the exercises on full stack open
+
+Link to the App on Render:
--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -73,6 +73,6 @@ app.post("/api/persons", (req, res) => {
   res.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
